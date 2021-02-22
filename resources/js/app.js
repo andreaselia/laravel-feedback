f0cbbbbafb63888e906e52f82a6375f2b3aee4f6
AndreasElia <andreaselia@live.co.uk>
2021-02-22 18:41:12 +0000

    feedback success screen and reset

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,13 +14,29 @@ createApp({
         text: '',
         screenshot: '',
       },
-      showFeedback: false
+      showFeedback: false,
+      submitted: false,
     }
   },
   template: `
     <div data-html2canvas-ignore>
       <div class="w-full shadow bg-white p-5 fixed top-0 sm:top-auto sm:right-5 sm:bottom-20 sm:max-w-sm sm:rounded-lg" v-if="showFeedback">
-        <form class="flex flex-col space-y-5" @submit.prevent="submit">
+        <div v-if="submitted">
+          <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+            <svg class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+            </svg>
+          </div>
+          <div class="mt-3 text-center sm:mt-5">
+            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+              Feedback Received
+            </h3>
+            <button type="button" @click="reset" class="mt-5 w-full px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Done
+            </button>
+          </div>
+        </div>
+        <form class="flex flex-col space-y-5" @submit.prevent="submit" v-else>
           <select v-model="form.type" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             <option value="" disabled>Select Type</option>
             <option value="idea">Idea</option>
@@ -32,7 +48,7 @@ createApp({
             <button type="button" @click="takeScreenshot" class="flex-1 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" v-if="!form.screenshot">
               Take Screenshot
             </button>
-            <button type="button" @click="removeScreenshot" class="flex-1 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" v-if="form.screenshot">
+            <button type="button" @click="removeScreenshot" class="flex-1 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" v-else>
               Remove Screenshot
             </button>
             <button type="submit" class="flex-1 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -58,8 +74,12 @@ createApp({
     },
     submit() {
       axios.post('/feedback', this.form)
-        .then((response) => console.log(response))
+        .then((response) => this.submitted = true)
         .catch((error) => console.error(error))
+    },
+    reset() {
+      this.showFeedback = false
+      this.submitted = false
     }
   }
 }).mount('#feedback')
